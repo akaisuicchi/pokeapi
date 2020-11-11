299251500fe60ad98f285207ccc49c0c94b952f6
Daniel Alvarez Vera <akaisuicchi@gmail.com>
2020-11-11 12:23:10 -0500

    Add RestTemplate configuration

---
+++ src/main/java/pokeapi/work/theapp/demo/configuration/RestTemplateConfiguration.java
@@ -0,0 +1,20 @@
+package pokeapi.work.theapp.demo.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+@Configuration
+public class RestTemplateConfiguration {
+    @Value("${work.theapp.external.pokeapi.uri}")
+    private String pokeApiUri;
+
+    @Bean
+    public RestTemplate pokeApiRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(this.pokeApiUri));
+        return restTemplate;
+    }
+}
